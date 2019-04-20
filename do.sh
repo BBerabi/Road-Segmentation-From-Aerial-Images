9f580dcb9c2a079df1a667fcb8aaa3d8df4a54f9
dcetin <dcetin@student.ethz.ch>
2019-04-20 07:42:38 +0200

    init do

--- do.sh
+++ do.sh
@@ -6,6 +6,8 @@ if [ "$OPTION" == "send" ]; then
     bsub -n 6 -W 4:00 -R "rusage[mem=1024, ngpus_excl_p=1]" python cil.py
 elif [ "$OPTION" == "watch" ]; then
     watch -n 1 bjobs
+elif [ "$OPTION" == "module" ]; then 
+	module load gcc/4.8.5 python_gpu/3.6.4 hdf5 eth_proxy
 else
     echo "Unknown option"
 fi
