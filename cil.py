c89e322a543d6c573201623cf411300b01085eff
dcetin <dcetin@student.ethz.ch>
2019-05-07 18:21:35 +0200

    data aug, pred resize

--- cil.py
+++ cil.py
@@ -7,6 +7,9 @@ import keras
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from PIL import Image
+from scipy import ndimage
+import random
+import argparse
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Activation, Dropout, Conv2D, MaxPooling2D, Conv2DTranspose
@@ -16,6 +19,15 @@ from mask_to_submission import mask_to_submission_strings, masks_to_submission
 os.environ['KAGGLE_USERNAME'] = ""
 os.environ['KAGGLE_KEY'] = ""
 FNULL = open(os.devnull, 'w')
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--verbose', action='store', dest='verbose', help='verbosity of the script', default=True, type=bool)
+argparser.add_argument('--batch-size', action='store', dest='batch_size', help='batch size for processing the samples', default=16, type=int)
+argparser.add_argument('--early-patience', action='store', dest='early_patience', help='patience for early stopping', default=25, type=int)
+argparser.add_argument('--epochs', action='store', dest='epochs', help='number of epochs', default=200, type=int)
+argparser.add_argument('--valid-split', action='store', dest='valid_split', help='percentage of validation examples', default=0.2, type=float)
+args = argparser.parse_args()
+verbose = args.verbose
 
 def download_data(verbose=False):
 	'''
@@ -65,7 +77,8 @@ def load_data():
 
 def submit_solution(fname='test', message='test'):
 	'''
-	Submits solution on kaggle.
+	Submits solution on kaggle. Command is as follows:
+	kaggle competitions submit -c cil-road-segmentation-2019 -f submission.csv -m "Message"
 	'''
 	submitbase = 'kaggle competitions submit -c cil-road-segmentation-2019 -f '
 	submit_all = submitbase + fname + ' -m "' + message + '"'
@@ -108,7 +121,10 @@ def build_model():
 	model.add(Activation('sigmoid'))
 	return model
 
-def pred_overlap(x_test, dim_train, dim_test):
+def pred_overlap(model, x_test, dim_train, dim_test):
+	'''
+	Make predictions by averaging over predictions of smaller overlapping segments.
+	'''
 	n_test = x_test.shape[0]
 	tl = x_test[:,:dim_train,:dim_train,:]
 	tr = x_test[:,:dim_train,-dim_train:,:]
@@ -131,17 +147,169 @@ def pred_overlap(x_test, dim_train, dim_test):
 	test_pred = np.divide(test_pred, pred_weights)
 	return test_pred
 
+def pred_resize(model, x_test, dim_train, dim_test):
+	'''
+	Downscales the images and upscales the predictions.
+	'''
+	n_test = x_test.shape[0]
+
+	x_test_downscaled = []
+	for i in range(n_test):
+		img_ds = cv2.resize(x_test[i], dsize=(dim_train, dim_train), interpolation=cv2.INTER_CUBIC)
+		x_test_downscaled.append(img_ds)
+	x_test_downscaled = np.asarray(x_test_downscaled)
+
+	test_pred_downscaled = model.predict(x_test_downscaled)
+
+	test_pred_upscaled = []
+	for i in range(n_test):
+		upscaled = cv2.resize(test_pred_downscaled[i], dsize=(dim_test, dim_test), interpolation=cv2.INTER_CUBIC)
+		test_pred_upscaled.append(img_ds)
+	test_pred_upscaled = np.asarray(test_pred_upscaled)
+
+	return test_pred_upscaled
+
+def augment_rotate_full(x_train, y_train):
+	'''
+	Augments the training data by rotating the images by right angles.
+	'''
+	x_aug = []
+	y_aug = []
+	for i in range(x_train.shape[0]):
+		for degree in [90,180,270]:
+			# degree = random.randint(0,361)
+			x_rot = ndimage.rotate(x_train[i], angle=degree, order=1, reshape=False, axes=(0,1))
+			y_rot = ndimage.rotate(y_train[i], angle=degree, order=1, reshape=False, axes=(0,1))
+			x_aug.append(x_rot)
+			y_aug.append(y_rot)
+	x_aug = np.array(x_aug)
+	y_aug = np.array(y_aug)
+	return x_aug, y_aug
+
+def augment_flip(x_train, y_train):
+	'''
+	Augments the training data by flipping the images.
+	'''
+	x_aug = []
+	y_aug = []
+	for i in range(x_train.shape[0]):
+		x_ud = np.flipud(x_train[i])
+		y_ud = np.flipud(y_train[i])
+		x_lr = np.fliplr(x_train[i])
+		y_lr = np.fliplr(y_train[i])
+		x_aug.append(x_ud)
+		y_aug.append(y_ud)
+		x_aug.append(x_lr)
+		y_aug.append(y_lr)
+	x_aug = np.array(x_aug)
+	y_aug = np.array(y_aug)
+	return x_aug, y_aug
+
+def augment_rotate_zoom(x_train, y_train, trials):
+	'''
+	Augments the training data by rotating the images arbitrarily, then zooming.
+	'''
+	crop_len = 60
+	x_aug = []
+	y_aug = []
+	for i in range(x_train.shape[0]):
+		for j in range(trials):
+			degree = random.randint(0,361)
+			rotated_img = ndimage.rotate(x_train[i], angle=degree, order=1, reshape=False, axes=(0,1))
+			cropped_img = rotated_img[crop_len:-crop_len,crop_len:-crop_len]
+			x_rot = cv2.resize(cropped_img, dsize=(400, 400), interpolation=cv2.INTER_CUBIC)
+			x_aug.append(x_rot)
+			rotated_img = ndimage.rotate(y_train[i], angle=degree, order=1, reshape=False, axes=(0,1))
+			cropped_img = rotated_img[crop_len:-crop_len,crop_len:-crop_len]
+			y_rot = cv2.resize(cropped_img, dsize=(400, 400), interpolation=cv2.INTER_CUBIC)
+			y_aug.append(y_rot)
+	x_aug = np.array(x_aug)
+	y_aug = np.array(y_aug)
+	y_aug = y_aug[:,:,:,np.newaxis]
+	return x_aug, y_aug
+
+def augment_crop_zoom(x_train, y_train, crop_len=100):
+	'''
+	Augments the training data by cropping and zooming the images.
+	'''
+	x_aug = []
+	y_aug = []
+	dim_train = x_train.shape[1]
+	if type(crop_len) == int:
+		crop_len = [crop_len for x in range(4)]
+	for i in range(x_train.shape[0]):
+		img = x_train[i]
+		cropped_img = img[crop_len[0]:-crop_len[1],crop_len[2]:-crop_len[3]]
+		x_crop = cv2.resize(cropped_img, dsize=(dim_train, dim_train), interpolation=cv2.INTER_CUBIC)
+		x_aug.append(x_crop)
+		img = y_train[i]
+		cropped_img = img[crop_len[0]:-crop_len[1],crop_len[2]:-crop_len[3]]
+		y_crop = cv2.resize(cropped_img, dsize=(dim_train, dim_train), interpolation=cv2.INTER_CUBIC)
+		y_aug.append(y_crop)
+	x_aug = np.array(x_aug)
+	y_aug = np.array(y_aug)
+	return x_aug, y_aug
+
+def hold_out_validation(x_train, y_train, valid_split=args.valid_split):
+	'''
+	Hold-out validation splitting.
+	'''
+	valid_split = np.floor(x_train.shape[0] * valid_split).astype(int)
+	valid_indices = np.random.choice(x_train.shape[0], valid_split)
+	train_indices = np.setdiff1d(np.arange(x_train.shape[0]), valid_indices)
+	x_valid = x_train[valid_indices]
+	x_train = x_train[train_indices]
+	y_valid = y_train[valid_indices]
+	y_train = y_train[train_indices]
+	return x_train, y_train, x_valid, y_valid
+
+def create_submission(test_pred, sub_fname='test_submission.csv'):
+	test_binary = np.ones(test_pred.shape)
+	test_binary[test_pred < 0.5] = 0
+	test_binary = test_binary.astype(int)
+	# Get file names
+	imgnames = [path_pred + fname for fname in sorted(os.listdir(path_test), key=lambda x: int( x[x.find('_')+1:x.find('.')] ))]
+	# Save prediction images
+	for i in range(len(imgnames)):
+		imarr = test_binary[i,:,:,0]
+		plt.imsave(imgnames[i], imarr, cmap=cm.gray)
+	# Create submission file
+	masks_to_submission(sub_fname, *imgnames)
+
 # Load train and test data
 path_train = './training/'
 path_test = './test_images/'
+path_pred = './pred_ims/'
 download_data()
 x_train, y_train, x_test = load_data()
 dim_test = x_test.shape[1]
 dim_train = x_train.shape[1]
+if verbose:
+	print('Loaded the data...')
 
-# Set hyperparameters
-batch_size = 16
-epochs = 200
+# Hold-out validation
+x_train, y_train, x_valid, y_valid = hold_out_validation(x_train, y_train, valid_split=0.1)
+
+# Augment the data
+
+# Symmetry
+x_flip, y_flip = augment_flip(x_train, y_train)
+print(x_train.shape, y_train.shape)
+print(x_flip.shape, y_flip.shape)
+x_train = np.concatenate((x_train, x_flip), axis=0)
+y_train = np.concatenate((y_train, y_flip), axis=0)
+
+# Rotation
+x_rot, y_rot = augment_rotate_full(x_train, y_train)
+x_aug, y_aug = augment_rotate_zoom(x_train, y_train, trials=3)
+print(x_train.shape, y_train.shape)
+print(x_rot.shape, y_rot.shape)
+print(x_aug.shape, y_aug.shape)
+
+x_train = np.concatenate((x_train, x_rot, x_aug), axis=0)
+y_train = np.concatenate((y_train, y_rot, y_aug), axis=0)
+if verbose:
+	print('Augmented the training data...')
 
 # Build and compile the model
 model = build_model()
@@ -152,40 +320,36 @@ model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 # Set callbacks
 file_checkpoint = "baseline_cnn_check.h5"
 checkpoint = ModelCheckpoint(file_checkpoint, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-early = EarlyStopping(monitor="val_acc", mode="max", patience=20, verbose=1)
+early = EarlyStopping(monitor="val_acc", mode="max", patience=args.early_patience, verbose=1)
 redonplat = ReduceLROnPlateau(monitor="val_acc", mode="max", patience=15, verbose=2)
 callbacks_list = [checkpoint, early]
+if verbose:
+	print('Compiled the model...')
 
 # Train the model
 model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0.1,
+          batch_size=args.batch_size,
+          epochs=args.epochs,
+          validation_data=(x_valid, y_valid),
           callbacks=callbacks_list,
           verbose=2,
           shuffle=True)
+if verbose:
+	print('Finished training...')
 
 # Load the checkpoint model
 model.load_weights(file_checkpoint)
 
 # Predict on test data
-test_pred = pred_overlap(x_test, dim_train, dim_test)
-test_binary = np.ones(test_pred.shape)
-test_binary[test_pred < 0.5] = 0
-test_binary = test_binary.astype(int)
+test_pred = pred_overlap(model, x_test, dim_train, dim_test)
+# test_pred = pred_resize(model, x_test, dim_train, dim_test)
+np.save('test_pred_holdout_aug.npy', test_pred)
 
-# Get file names
-path_pred = './pred_ims/'
-imgnames = [path_pred + fname for fname in sorted(os.listdir(path_test), key=lambda x: int( x[x.find('_')+1:x.find('.')] ))]
-
-# Save prediction images
-for i in range(len(imgnames)):
-	imarr = test_binary[i,:,:,0]
-	plt.imsave(imgnames[i], imarr, cmap=cm.gray)
-
-# Create submission file
-submission_filename = 'test_submission.csv'
-masks_to_submission(submission_filename, *imgnames)
-
+# Create submission
+sub_fname = 'submission_holdout_aug.csv'
+create_submission(test_pred, sub_fname=sub_fname)
+if verbose:
+	print('Created the submission file...')
 # Send submission file
 # submit_solution(fname=submission_filename, message='test')
+
