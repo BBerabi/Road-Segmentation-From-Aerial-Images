62ff3615a7fc4202c6ae041727f13958c3126d77
himeva <himeva@student.ethz.ch>
2019-06-19 02:46:10 +0200

    initial commit

---
+++ bad-meva-unet/metrics.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import keras.backend as K
+import os
+import skimage.io as io
+# credits: https://stackoverflow.com/questions/43547402/how-to-calculate-f1-macro-in-keras
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+# credits: https://github.com/lyakaap/Kaggle-Carvana-3rd-Place-Solution/blob/master/losses.py
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+def save_result(save_path, npyfile):
+    
+    if not os.path.exists(save_path):
+            os.makedirs(save_path)
+    for i, item in enumerate(npyfile):
+        img = item[:,:,0]
+        io.imsave(os.path.join(save_path, '%.3d.png'%(i+1)), img)
+    print("Saved submission.")
+from keras.losses import binary_crossentropy
+
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss(y_true, y_pred):
+    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
